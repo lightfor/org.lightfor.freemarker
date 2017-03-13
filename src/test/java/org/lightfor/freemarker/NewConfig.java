808d0eda772d4c44beaf3d81b87f90f354ed8870
lightfor <death_light@foxmail.com>
2017-03-13 19:56:08 +0800

    sample code

---
+++ src/test/java/org/lightfor/freemarker/NewConfig.java
@@ -0,0 +1,55 @@
+package org.lightfor.freemarker;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * sample from official web
+ * Created by Light on 2016/9/30.
+ */
+public class NewConfig {
+
+    @Test
+    public void test() throws IOException, TemplateException {
+        // Create your Configuration instance, and specify if up to what FreeMarker
+        // version (here 2.3.25) do you want to apply the fixes that are not 100%
+        // backward-compatible. See the Configuration JavaDoc for details.
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
+
+        // Specify the source where the template files come from. Here I set a
+        // plain directory for it, but non-file-system sources are possible too:
+        cfg.setDirectoryForTemplateLoading(new File("."));
+
+        // Set the preferred charset template files are stored in. UTF-8 is
+        // a good choice in most applications:
+        cfg.setDefaultEncoding("UTF-8");
+
+        // Sets how errors will appear.
+        // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+
+        // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+        cfg.setLogTemplateExceptions(false);
+
+        Map<String, String> root = new HashMap<>();
+        root.put("user", "Big Joe");
+        root.put("url", "products/greenmouse.html");
+        root.put("name", "green mouse");
+
+        Template temp = cfg.getTemplate("test.ftlh");
+
+        Writer out = new OutputStreamWriter(System.out);
+        temp.process(root, out);
+
+    }
+}
